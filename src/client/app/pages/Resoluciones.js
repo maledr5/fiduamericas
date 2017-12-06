d210b7709f170db3db9936e6514db3a38709a611
Maleh Duenas <maledr5@hotmail.es>
2017-12-06 14:42:32 -0500

    adds new file in resoluciones

--- src/client/app/pages/Resoluciones.js
+++ src/client/app/pages/Resoluciones.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 import resoluciones_01 from '../data/downloads/resoluciones_01.pdf'
 import resoluciones_02 from '../data/downloads/resoluciones_02.pdf'
+import resoluciones_03 from '../data/downloads/resoluciones_03.pdf'
 
 
 const Resoluciones = () => {
@@ -27,6 +28,10 @@ const Resoluciones = () => {
 					<div className="row link-row">
 						<span className="col offset-s1 pdf-icon"></span>
 						<a href={resoluciones_02} className="" target="_blank">Resolución 20135</a>
+					</div>
+					<div className="row link-row">
+						<span className="col offset-s1 pdf-icon"></span>
+						<a href={resoluciones_03} className="" target="_blank">Resolución 23710</a>
 					</div>
 				</div>
 			</div>
