a4ebcaed6386da30c11a993e0283f154e752a920
Maleh Duenas <maledr5@hotmail.es>
2017-12-19 19:59:14 -0500

    Changes in FAQ: adds mercantil to fideicomisos and icons to fondos

--- src/client/app/data/faq/como_puedo_invertir.js
+++ src/client/app/data/faq/como_puedo_invertir.js
@@ -2,17 +2,38 @@ import React from 'react';
 
 import FaqQuestion from '../../components/FaqQuestion.js'
 
+import paso1 from '../../images/faq/paso1.svg';
+import paso2 from '../../images/faq/paso2.svg';
+import paso3 from '../../images/faq/paso3.svg';
+import paso4 from '../../images/faq/paso4.svg';
+import paso5 from '../../images/faq/paso5.svg';
+
 
 const question = "¿Cómo puedo invertir?";
 
 const answer =
 <div>
 	<ul className="no-bullets">
-		<li><strong>Paso 1:</strong> Asesórate, nuestro objetivo es conocer tus necesidades y expectativas para poder ofrecerte el tipo de fondo que más te convenga. </li>
-		<li><strong>Paso 2:</strong> Una vez que hayas escogido en qué fondo quieres invertir, tienes que llenar unos formularios con información como tu cédula, tu domicilio y tus ingresos. Firmamos el contrato en el cual se especifica el reglamento interno de tu fondo. </li>
-		<li><strong>Paso 3:</strong> Verificamos tus datos y nuestro oficial de cumplimiento procede a dar el visto bueno sobre tu expediente.  </li>
-		<li><strong>Paso 4:</strong> Invierte, tienes que transferir o depositar en nuestras cuentas bancarias los correspondientes montos. </li>
-		<li><strong>Paso 5:</strong> Espera y gana. Nosotros nos ocupamos del resto. </li>
+		<li className="faq-icon-item">
+			<img src={ paso1 } className="faq-icon" />
+			<p> <strong>Paso 1:</strong> Asesórate, nuestro objetivo es conocer tus necesidades y expectativas para poder ofrecerte el tipo de fondo que más te convenga.  </p> 
+		</li>
+		<li className="faq-icon-item">
+			<img src={ paso2 } className="faq-icon" />
+			<p> <strong>Paso 2:</strong> Una vez que hayas escogido en qué fondo quieres invertir, tienes que llenar unos formularios con información como tu cédula, tu domicilio y tus ingresos. Firmamos el contrato en el cual se especifica el reglamento interno de tu fondo.  </p> 
+		</li>
+		<li className="faq-icon-item">
+			<img src={ paso3 } className="faq-icon" />
+			<p> <strong>Paso 3:</strong> Verificamos tus datos y nuestro oficial de cumplimiento procede a dar el visto bueno sobre tu expediente.   </p> 
+		</li>
+		<li className="faq-icon-item">
+			<img src={ paso4 } className="faq-icon" />
+			<p> <strong>Paso 4:</strong> Invierte, tienes que transferir o depositar en nuestras cuentas bancarias los correspondientes montos.  </p> 
+		</li>
+		<li className="faq-icon-item">
+			<img src={ paso5 } className="faq-icon" />
+			<p> <strong>Paso 5:</strong> Espera y gana. Nosotros nos ocupamos del resto.  </p> 
+		</li>
 	</ul>
 </div>;
 
--- src/client/app/data/faq/cuales_son_los_beneficios_de_invertir_en_un_fondo_de_inversion.js
+++ src/client/app/data/faq/cuales_son_los_beneficios_de_invertir_en_un_fondo_de_inversion.js
@@ -2,20 +2,53 @@ import React from 'react';
 
 import FaqQuestion from '../../components/FaqQuestion.js'
 
+import administracionProfesional from '../../images/faq/administracionProfesional.svg';
+import rendimiento from '../../images/faq/rendimiento.svg';
+import diversificacion from '../../images/faq/diversificacion.svg';
+import liquidez from '../../images/faq/liquidez.svg';
+import economiaEscala from '../../images/faq/economiaEscala.svg';
+import reinversion from '../../images/faq/reinversion.svg';
+import regulacion from '../../images/faq/regulacion.svg';
+import montos from '../../images/faq/paso4.svg';
+
 
 const question = "¿Cuáles son los beneficios de invertir en un fondo de inversión?";
 
 const answer =
 <div>
 	<ul>
-		<li><strong>Administración profesional:</strong> Un inversionista común se enfrenta al desconocimiento de las técnicas que ayudan a optimizar el manejo de un portafolio de inversión y que ayudan a realizar una correcta administración de riesgos. </li>
-		<li><strong>Rendimiento:</strong> Los fondos de inversión son más rentables que la mayoría de instrumentos financieros en el mercado.</li>
-		<li><strong>Diversificación:</strong> El riesgo sistemático es el que podríamos llamar el riesgo inherente a un mercado. Por el contrario, el riesgo no sistemático es el riesgo particular de cada emisora. El riesgo no sistemático se dice que es diversificable, porque se puede reducir o controlar con una diversificación adecuada.</li>
-		<li><strong>Liquidez:</strong> Los fondos de inversión ofrecen diferentes plazos de liquidación desde el momento en que el inversionista solicita la venta de los mismos hasta que recibe el dinero. </li>
-		<li><strong>Economía de escala:</strong> Los costos involucrados en el manejo de un fondo de inversión se reparten entre todos los accionistas del mismo, lo cual hace accesible a los pequeños inversionistas el manejo profesional de sus inversiones.</li>
-		<li><strong>Reinversión:</strong> Fiduamericas reinvierte el dinero recibido por cobro de interés, amortizaciones y dividendos, lo que permite que capitalicen continuamente los recursos del fondo. El inversionista no se tiene que preocupar de renovar su inversión al invertir en un fondo de inversión.</li>
-		<li><strong>Montos de inversión:</strong> En el mercado existe una gran cantidad de fondos de inversión en los que se puede participar mediante montos bajos, obteniendo los beneficios que ofrecen estos instrumentos de inversión.</li>
-		<li><strong>Regulación y supervisión:</strong> Los fondos de inversión, así como los prestadores de servicios correspondientes, están sujetos a la legislación ecuatoriana como la Ley de Mercado de Valores, regulaciones que tienen como objetivo proteger los intereses del público inversionista mediante la supervisión y vigilancia de la Superintendencia de Compañias, Seguros y Valores.</li>
+		<li className="faq-icon-item">
+			<img className="faq-icon" src={administracionProfesional} />
+			<p><strong>Administración profesional:</strong> Un inversionista común se enfrenta al desconocimiento de las técnicas que ayudan a optimizar el manejo de un portafolio de inversión y que ayudan a realizar una correcta administración de riesgos. </p>
+		</li>
+		<li className="faq-icon-item">
+			<img className="faq-icon" src={rendimiento} />
+			<p><strong>Rendimiento:</strong> Los fondos de inversión son más rentables que la mayoría de instrumentos financieros en el mercado.</p>
+		</li>
+		<li className="faq-icon-item">
+			<img className="faq-icon" src={diversificacion} />
+			<p><strong>Diversificación:</strong> El riesgo sistemático es el que podríamos llamar el riesgo inherente a un mercado. Por el contrario, el riesgo no sistemático es el riesgo particular de cada emisora. El riesgo no sistemático se dice que es diversificable, porque se puede reducir o controlar con una diversificación adecuada.</p>
+		</li>
+		<li className="faq-icon-item">
+			<img className="faq-icon" src={liquidez} />
+			<p><strong>Liquidez:</strong> Los fondos de inversión ofrecen diferentes plazos de liquidación desde el momento en que el inversionista solicita la venta de los mismos hasta que recibe el dinero. </p>
+		</li>
+		<li className="faq-icon-item">
+			<img className="faq-icon" src={economiaEscala} />
+			<p><strong>Economía de escala:</strong> Los costos involucrados en el manejo de un fondo de inversión se reparten entre todos los accionistas del mismo, lo cual hace accesible a los pequeños inversionistas el manejo profesional de sus inversiones.</p>
+		</li>
+		<li className="faq-icon-item">
+			<img className="faq-icon" src={reinversion} />
+			<p><strong>Reinversión:</strong> Fiduamericas reinvierte el dinero recibido por cobro de interés, amortizaciones y dividendos, lo que permite que capitalicen continuamente los recursos del fondo. El inversionista no se tiene que preocupar de renovar su inversión al invertir en un fondo de inversión.</p>
+		</li>
+		<li className="faq-icon-item">
+			<img className="faq-icon" src={montos} />
+			<p><strong>Montos de inversión:</strong> En el mercado existe una gran cantidad de fondos de inversión en los que se puede participar mediante montos bajos, obteniendo los beneficios que ofrecen estos instrumentos de inversión.</p>
+		</li>
+		<li className="faq-icon-item">
+			<img className="faq-icon" src={regulacion} />
+			<p><strong>Regulación y supervisión:</strong> Los fondos de inversión, así como los prestadores de servicios correspondientes, están sujetos a la legislación ecuatoriana como la Ley de Mercado de Valores, regulaciones que tienen como objetivo proteger los intereses del público inversionista mediante la supervisión y vigilancia de la Superintendencia de Compañias, Seguros y Valores.</p>
+		</li>
 	</ul>
 </div>;
 
