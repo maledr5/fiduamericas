de4daab8c9db350457d9251af1ddd9c02a94ea3b
Maleh Duenas <maledr5@gmail.com>
2019-06-16 15:18:12 -0500

    Adds logic to calculator fondo fit

--- src/client/app/components/CalculadoraFondos/CalculadoraFondos.js
+++ src/client/app/components/CalculadoraFondos/CalculadoraFondos.js
@@ -16,28 +16,78 @@ class ContactoForm extends React.Component {
             calcularRendimiento: "Calcular Rendimiento"
         }
     }
-    
+
+    INSERT_ALL_VALUES_ERROR = "Debe ingresar todos los campos antes de calcular.";
+    TASA_FONDO_FIT = 0.0535;
+    PERIOD_IN_DAYS = 30
+    YEAR_IN_DAYS = 360
 
     constructor(props) {
 		super(props);
 		this.state = {
             montoInicial: "",
-            permanencia: "",
-            incrementoMensual: ""
+            permanencia: 90,
+            incrementoMensual: 10,
+            totalRecibir: "",
+            rendimientoFondo: "",
+            rendimientoBanco: ""
 		};
     }
-    
+
+    round(number) {
+        number = parseFloat(number)
+        return number.toFixed(2)
+    }
+
     updateValue(event) {
         var targetName = event.target.name;
         var targetValue = event.target.value;
-        console.log('targetName: ', targetName)
 		this.setState({[targetName]: targetValue});
-	}
+    }
+
+    calcularRetornoMensual(incrementalAmount, time, foundRate) {
+        var remainingTime = time - this.PERIOD_IN_DAYS;
+        var totalretornoMensual = 0
+        do {
+          var retornoMensual = incrementalAmount * foundRate * remainingTime / this.YEAR_IN_DAYS
+          totalretornoMensual += retornoMensual
+          remainingTime = remainingTime - this.PERIOD_IN_DAYS
+        } while (remainingTime > 0)
+        return totalretornoMensual
+    }
+
+    showResults(montoInicial, permanencia, incrementoMensual) {
+        const retornoMensual = this.calcularRetornoMensual(incrementoMensual, permanencia, this.TASA_FONDO_FIT)
+        const retornoAnualDeMontoInicial = montoInicial * this.TASA_FONDO_FIT * permanencia / this.YEAR_IN_DAYS
+
+        const monthlyStay = Math.floor(permanencia / this.PERIOD_IN_DAYS)
+
+        const totalAmount = parseFloat(montoInicial) +
+        parseFloat(incrementoMensual * monthlyStay) +
+        parseFloat(retornoMensual) +
+        parseFloat(retornoAnualDeMontoInicial)
+
+        const totalReturn = retornoMensual + retornoAnualDeMontoInicial
+        const banckReturn = 0
+
+        this.setState({["totalRecibir"]: this.round(totalAmount)});
+        this.setState({["rendimientoFondo"]: this.round(totalReturn)});
+        this.setState({["rendimientoBanco"]: this.round(banckReturn)});
+    }
+
+    calculate(event) {
+        event.preventDefault()
+        const {montoInicial, permanencia, incrementoMensual} = this.state
+        this.showResults(montoInicial, permanencia, incrementoMensual)
+    }
 
     render() {
-        const {montoInicial, permanencia, incrementoMensual} = this.state
+        const {montoInicial, permanencia, incrementoMensual, error, totalRecibir, rendimientoFondo, rendimeintoBanco} = this.state
         return (
                 <form>
+                    <div className="row errorNotification">
+                        {error}
+                    </div>
                     <div className="row">
                         <div className="col s12 l6">
                             <label htmlFor={this.CONSTANT.id.montoInicial}>{this.CONSTANT.nombre.montoInicial}</label>
@@ -47,7 +97,7 @@ class ContactoForm extends React.Component {
                                 value={montoInicial}
                                 onChange={event => this.updateValue(event)}
                                 name={this.CONSTANT.id.montoInicial}
-                                id={this.CONSTANT.id.montoInicial}    
+                                id={this.CONSTANT.id.montoInicial}
                                 required="required"
                             />
                         </div>
@@ -83,12 +133,13 @@ class ContactoForm extends React.Component {
                                 type="submit"
                                 name="footer-submit"
                                 value={this.CONSTANT.nombre.calcularRendimiento}
+                                onClick={event => this.calculate(event)}
                             />
                         </div>
                     </div>
-                    <h2>Valor a recibir: </h2>
-                    <h4>Rendimiento obtenido en el Fondo Fit: </h4>
-                    <h4>Rendimiento que obtendrías en un banco: </h4>
+                    <h2 className="text-main">Valor a recibir: {totalRecibir} </h2>
+                    <h4>Rendimiento obtenido en el Fondo Fit: {rendimientoFondo}</h4>
+                    {/* <h4>Rendimiento que obtendrías en un banco: {rendimeintoBanco}</h4> */}
                 </form>
         )
     }
